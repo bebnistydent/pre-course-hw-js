5432062b1310dc876ba22ff7ddc092ed5c5a04c1
Nikita Yerameev <Smertskosoii@gmail.com>
2023-12-15 13:17:05 +0300

    Complited sprint 00, week 1, JS - task 02

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,6 @@
-// code
+let number1 = Math.floor(Math.random() * 100)
+  if (number1 % 2 === 0) {
+    console.log(`${number1} это четное число`);
+  } else if (number1 % 2 !== 0) {
+    console.log(`${number1} это не четное число`)
+  }
